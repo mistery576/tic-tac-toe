02ef1b0179d962520395b0e4e113908757959cc1
Miguel Costa <113616978+mistery576@users.noreply.github.com>
2022-10-18 15:57:52 +0100

    v 1.2
    The javascript its almost done.
    - When we have a Winner, we can still playing.
    - Script for the button.

--- script.js
+++ script.js
@@ -5,33 +5,91 @@
 // Imagem X = https://icones.pro/wp-content/uploads/2021/08/icone-x-noir.png
 // Imagam O = https://static.vecteezy.com/system/resources/previews/001/192/291/original/circle-png.png
 
-var casa1 = document.getElementById('casa1');
-var casa2 = document.getElementById('casa2');
-var casa3 = document.getElementById('casa3');
-var casa4 = document.getElementById('casa4');
-var casa5 = document.getElementById('casa5');
-var casa6 = document.getElementById('casa6');
-var casa7 = document.getElementById('casa7');
-var casa8 = document.getElementById('casa8');
-var casa9 = document.getElementById('casa9');
-var casa =  document.getElementsByClassName('casa');
-
 var jogador = 1;
 
-function hello (n){
-    var symbol = n.style.backgroundImage;
+function hello (n){ 
+    var casa_escolhida = document.getElementById("casa" + n);
+    var symbol = casa_escolhida.style.backgroundImage;
     
     if(jogador == 1 && symbol == ''){
-        n.style.backgroundImage = "url('https://static.vecteezy.com/system/resources/previews/001/192/291/original/circle-png.png')";
+        casa_escolhida.style.backgroundImage = "url('https://static.vecteezy.com/system/resources/previews/001/192/291/original/circle-png.png')";
         jogador++; 
     }
     else if(jogador == 2 && symbol == ''){
-        n.style.backgroundImage = "url('https://icones.pro/wp-content/uploads/2021/08/icone-x-noir.png')";
+        casa_escolhida.style.backgroundImage = "url('https://icones.pro/wp-content/uploads/2021/08/icone-x-noir.png')";
         jogador--;
     }
     else {
         console.log('deu erro');
+        console.log(symbol);
     }
-
+    
     let turno = document.getElementById('vez-jogador').innerHTML = "Movimento do jogador " + jogador;
+    verify_victory ();
+    show_victory ();
+}
+
+function verify_victory () {
+    var bg1 = document.getElementById('casa1').style.backgroundImage;
+    var bg2 = document.getElementById('casa2').style.backgroundImage;
+    var bg3 = document.getElementById('casa3').style.backgroundImage;
+    var bg4 = document.getElementById('casa4').style.backgroundImage;
+    var bg5 = document.getElementById('casa5').style.backgroundImage;
+    var bg6 = document.getElementById('casa6').style.backgroundImage;
+    var bg7 = document.getElementById('casa7').style.backgroundImage;
+    var bg8 = document.getElementById('casa8').style.backgroundImage;
+    var bg9 = document.getElementById('casa9').style.backgroundImage;
+    
+    if (bg1 != '' && bg2 != '' && bg3 != ''){
+        if (bg1 == bg2 && bg1 == bg3 && bg2 == bg3){
+            console.log('bruh');
+            return 1;
+        }    
+    }
+    if (bg4 != '' && bg5 != '' && bg6 != ''){
+        if (bg4 == bg5 && bg4 == bg6 && bg5 == bg6){
+            console.log('bruh');
+        }
+    }
+    if (bg7 != '' && bg8 != '' && bg9 != ''){
+        if (bg7 == bg8 && bg7 == bg9 && bg8 == bg9){
+            console.log('bruh');
+        }
+    }
+    if (bg1 != '' && bg4 != '' && bg7 != ''){
+        if (bg1 == bg4 && bg1 == bg7 && bg4 == bg7){
+            console.log('bruh');
+        }
+    }
+    if (bg2 != '' && bg5 != '' && bg8 != ''){
+        if (bg2 == bg5 && bg2 == bg8 && bg5 == bg8){
+            console.log('bruh');
+        }
+    }
+    if (bg3 != '' && bg6 != '' && bg9 != ''){
+        if (bg3 == bg6 && bg3 == bg9 && bg6 == bg9){
+            console.log('bruh');
+        }
+    }
+    if (bg1 != '' && bg5 != '' && bg9 != ''){
+        if (bg1 == bg5 && bg1 == bg9 && bg5 == bg9){
+            console.log('bruh');
+        }
+    }
+    if (bg3 != '' && bg5 != '' && bg7 != ''){
+        if(bg3 == bg5 && bg3 == bg7 && bg5 == bg7){
+            console.log('bruh');
+        }
+    }
+}
+
+function show_victory (){
+    var text = document.createTextNode("O vencedor é o jogador " + jogador);
+    var titulo = document.getElementById('show_result');
+    
+    var casas = document.getElementsByClassName('casa');
+
+    if (verify_victory() == 1){
+        titulo.appendChild(text);
+    }
 }
